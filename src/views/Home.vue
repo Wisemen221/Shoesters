062fac184c230197898816371c54e71111abbf5f
jake <theronbogan1@gmail.com>
2021-03-05 21:02:47 -0500

    update 0.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    Home
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home'
+    }
 </script>
+
+<style>
+    .home-wrapper{
+        position: relative;
+        width: 100%;
+        max-height: 100%;
+        height: 100%;
+        overflow-y: auto;
+    }
+</style>
