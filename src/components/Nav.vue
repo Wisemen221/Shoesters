b7870eba2745c99d70428cdbbb1e4530ec624d7f
jake <theronbogan1@gmail.com>
2021-03-08 21:25:25 -0500

    update 0.4

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,15 @@
 <template>
-    <header class="header">
+   
+<!--
+header******
+-->
+    <!-- changing the background of the header on scroll -->
+    <header class="header" :class="change_color ? 'no_background' : 'red_background'">
         <nav class="header_nav flex flex_jc_sb flex_ai_c">
-            
             <!-- logo image -->
             <a href="/" class="header_logo flex flex_ai_c">
                 <img src="@/assets/images/svg/logo.svg" alt="Shoesters Logo">
             </a>
-
             <!-- mobile menu button -->
             <a href="#" ref="mobileToggle" @click="toggleOn" class="header_mobile_toggle">
                 <!-- using the span elements to create the mobile menu design -->
@@ -14,21 +17,20 @@
                 <span></span>
                 <span></span>
             </a>
-
             <!-- social media buttons -->
             <div class="header_social_links">
                 <a href="https://www.facebook.com/Shoesters/"><i class="fab fa-facebook"></i></a>
                 <a href="https://www.instagram.com/shoesters1358/"><i class="fab fa-instagram"></i></a>
             </div>
-
         </nav>
     </header>
+    
 </template>
 
 <script>
     import { ref } from 'vue'
     export default {
-        props:['toggled'],
+        props:['toggled','change_color'],
         setup(props,context){
             const mobileToggle = ref(null)
             const toggleOn = ()=>{
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,61 @@
 <template>
-    <!-- ***
-    navbar component
-    *** -->
-    <Nav :toggled="toggled" @close_menu="layoverToggle" @open_menu="toggleLayover"/>
+<!--
+nav******
+-->
+    <Nav :change_color="change_color" :toggled="toggled" @close_menu="layoverToggle" @open_menu="toggleLayover"/>
 
-    <!-- ***
-    containing everything in webpage in order for the user to scroll.
-    For some reason a fixed navbar will not work in Vue 3
-    *** -->
-    <div class="scroll_wrapper">
+<!--
+scroll wrappper******
+-->
+    <!-- containing everything in webpage in order for the user to scroll. For some reason a fixed navbar will not work in Vue 3 -->
+    <div ref="scrolling_element" @scroll="scrolling" class="scroll_wrapper">
 
-        <!-- ***
-        mobile menu that appears for mobile devices
-        *** -->
-        <div ref="layover" class="layover_container" v-if="toggled">
-
+<!--
+layover******
+-->
+        <!-- mobile menu that appears for mobile devices -->
+        <div class="layover" v-if="toggled">
+            <a href="#">HOME</a>
+            <a href="#">ABOUT</a>
+            <a href="#">CAREERS</a> <!-- external link to company's career site --> 
+            <a href="#">CONTACT</a>
         </div>
 
-        <!-- ***
-        the landing page
-        *** -->
-        <section class="hero_container">
-
+<!-- 
+hero******
+-->
+        <!-- the landing page -->
+        <section class="hero">
+            <div class="hero_content">
+                <!-- contains the nav and image content -->
+                <div class="hero_nav">
+                    <h1>Purveyors of Comfortable<br>Luxury Footwear</h1>
+                    <!-- nav links for desktop only -->
+                    <!-- animated nav section with gsap animations -->
+                    <nav class="hero_links">
+                        <div class="left_nav">
+                            <ul>
+                                <li><a href="#">HOME</a></li>
+                                <li><a href="#">ABOUT</a></li>
+                            </ul>
+                        </div>
+                        <div class="right_nav"> <!-- animated border that splits the nav -->
+                            <ul>
+                                <li><a href="#">CAREERS</a></li>
+                                <li><a href="#">CONTACT</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <!-- button link that goes to the contact section -->
+                    <a class="hero_button" href="#">LET'S TALK</a>
+                </div>
+                <div class="hero_image">
+                    <!-- the boot -->
+                    <img src="@/assets/images/png/shoe_11.png" aria-label="A single blue boot" alt="One single boot">
+                </div>
+            </div>
+            <!-- bottom border -->
+            <img class="hero_border" src="@/assets/images/svg/border_1.svg" role="presentation">
         </section>
 
     </div>
@@ -30,13 +64,14 @@
 
 <script>
     import Nav from "@/components/Nav"
-    import { onUpdated, ref } from 'vue'
+    import { ref } from 'vue'
 
     export default {
         name: 'Home',
         components:{Nav},
         setup(props,context){
-            const layover = ref(null)
+            const scrolling_element = ref(null) //scroll wrapper element that is being scrolled by user
+            const change_color = ref(false) //variable that's passed to nav component for background change
             const toggled = ref(false)
             const toggleLayover = ()=>{ //allows mobile menu to open
                 toggled.value = true
@@ -44,26 +79,27 @@
             const layoverToggle = ()=>{ //allows mobile menu to close
                 toggled.value = false
             }
-            console.log(layover)
-            const test = onUpdated(()=>{
-                if(toggled.value){
-                    console.log(true)/// try using watch to see if you add the fade out animation!!**************
+            const scrolling = ()=>{ //detecting if the user scrolled to the top of page
+                if(scrolling_element.value.scrollTop >= 80){
+                    //scrolls down
+                    change_color.value = false
+                }else if(scrolling_element.value.scrollTop <= 80){
+                    //scrolls up
+                    change_color.value = true
                 }
-            })
-            return{toggled,toggleLayover,layoverToggle,layover}
-        }
+            }
+            return{toggled,toggleLayover,layoverToggle,scrolling,scrolling_element,change_color}
+        },
     }
 </script>
 
 <style>
-    /* ***
-    div that contains all that can be scrolled
-    *** */
+    /* div that contains all that can be scrolled */
     .scroll_wrapper{
         position: relative;
         max-height: 100%;
         width: 100vw;
         height: 100%;
-        overflow-y: auto; /* ***allows the page to scroll*** */
+        overflow-y: auto; /* allows the page to scroll */
     }
 </style>
