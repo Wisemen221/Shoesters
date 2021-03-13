e51af1aaf3cacd7a1cf2f1fcb6d1551de4bf7bdf
jake <theronbogan1@gmail.com>
2021-03-12 20:45:22 -0500

    update 1.0

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCXrhytIhuhpIjGmWNZ2P6ablhEh8a1Skk",
-//   authDomain: "portfolio-2355c.firebaseapp.com",
-//   projectId: "portfolio-2355c",
-//   storageBucket: "portfolio-2355c.appspot.com",
-//   messagingSenderId: "686324150549",
-//   appId: "1:686324150549:web:21c9347d9bbcab58d8f91c"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyB_EW9eyjsA3wemwmjNq5P99y5i0Y2KXLU",
+    authDomain: "shoesters-d25da.firebaseapp.com",
+    projectId: "shoesters-d25da",
+    storageBucket: "shoesters-d25da.appspot.com",
+    messagingSenderId: "396562714210",
+    appId: "1:396562714210:web:19d19cb9fd9423b8710b3b"
+  };
 
 // init firebase
 firebase.initializeApp(firebaseConfig)
